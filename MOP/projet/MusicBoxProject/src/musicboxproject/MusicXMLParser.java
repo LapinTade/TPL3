cd0a782cbc41ddccce89691823bf6c1d68288766
LapinTade <nroze@lx17.enseignement.lifc.univ-fcomte.fr>
2013-02-12 10:43:40 +0100

    Ajout du projet de MOP

---
+++ MOP/projet/MusicBoxProject/src/musicboxproject/MusicXMLParser.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicboxproject;
+
+/**
+ *
+ * @author nroze
+ */
+public class MusicXMLParser {
+    
+}
